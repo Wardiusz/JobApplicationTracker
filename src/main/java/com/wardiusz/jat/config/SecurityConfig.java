9205b5827b7cb9ab605975f06299787a1b21798c
Łukasz <123584186+Wardiusz@users.noreply.github.com>
2026-05-04 02:18:42 +0200

    fix: fix typo with elements in list

--- src/main/java/com/wardiusz/jat/config/SecurityConfig.java
+++ src/main/java/com/wardiusz/jat/config/SecurityConfig.java
@@ -66,7 +66,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:4200, https://jat-demo.up.railway.app"));
+        config.setAllowedOrigins(List.of("http://localhost:4200", "https://jat-demo.up.railway.app"));
         config.setAllowedMethods(List.of("GET","POST","PUT","PATCH","DELETE","OPTIONS"));
         config.setAllowedHeaders(List.of("Set-Cookie"));
         config.setAllowedHeaders(List.of("*"));
