3d62d6e8cacce2d17902671b4dc1686030f57aed
Łukasz <123584186+Wardiusz@users.noreply.github.com>
2026-05-12 19:23:47 +0200

    refactor: add the jakarta annotations for validation

--- src/main/java/com/wardiusz/jat/dto/request/OtpRequest.java
+++ src/main/java/com/wardiusz/jat/dto/request/OtpRequest.java
@@ -1,5 +1,8 @@
 package com.wardiusz.jat.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,9 +13,11 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class OtpRequest {
-
+    @Size(min = 6, max = 6, message = "OTP must be 6 digits")
     private String otp;
 
+    @NotBlank
+    @Email
     private String email;
 
 }
