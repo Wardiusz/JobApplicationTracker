5e1eeaf2e02c478b0ab461b3859194f3585e7b8d
Łukasz <123584186+Wardiusz@users.noreply.github.com>
2026-05-14 00:56:12 +0200

    fix: delete not used origin

--- src/main/java/com/wardiusz/jat/config/SecurityConfig.java
+++ src/main/java/com/wardiusz/jat/config/SecurityConfig.java
@@ -66,7 +66,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:4200", "https://jat-demo.wardiusz.dev"));
+        config.setAllowedOrigins(List.of("https://jat-demo.wardiusz.dev"));
         config.setAllowedMethods(List.of("GET","POST","PUT","PATCH","DELETE","OPTIONS"));
         config.setAllowedHeaders(List.of("Set-Cookie"));
         config.setAllowedHeaders(List.of("*"));
