40e863fbc6c7327a770605097a7612b7018a8367
Łukasz <123584186+Wardiusz@users.noreply.github.com>
2026-07-29 18:36:28 +0200

    refractor: change the name of the class to more compact one

--- backend/src/main/java/com/wardiusz/jat/JobTrackerApplication.java
+++ backend/src/main/java/com/wardiusz/jat/JobTrackerApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JobApplicationTrackerApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(JobApplicationTrackerApplication.class, args);
+public class JobTrackerApplication {
+    static void main(String[] args) {
+        SpringApplication.run(JobTrackerApplication.class, args);
     }
-
 }
